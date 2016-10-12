6135192a810a10e4bc88f0858a368b989de8a720
fetorres <fetorres@groupon.com>
2016-10-12 11:58:02 -0300

    update

--- js/main.js
+++ js/main.js
@@ -25,10 +25,9 @@ jQuery(function($) {
 
 		$('header').height($(window).height() + 80);
 		$('section .cut').each(function() {
-			if ($(this).hasClass('cut-top'))
-				$(this).css('border-right-width', $(this).parent().width() + "px");
-			else if ($(this).hasClass('cut-bottom'))
+			if ($(this).hasClass('cut-bottom')){
 				$(this).css('border-left-width', $(this).parent().width() + "px");
+			}
 		});
 
 		// Sliders Init
@@ -152,7 +151,7 @@ jQuery(function($) {
 			bottomMargin = parseInt($dialog.css('marginBottom'), 10);
 
 		// Make sure you don't hide the top part of the modal w/ a negative margin
-		// if it's longer than the screen height, and keep the margin equal to 
+		// if it's longer than the screen height, and keep the margin equal to
 		// the bottom margin of the modal
 		if (offset < bottomMargin) offset = bottomMargin;
 		$dialog.css("margin-top", offset);
